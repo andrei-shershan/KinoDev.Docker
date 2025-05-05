0d015978cce23c4c2b3d8eb6ab9d593c1b30b675
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2025-05-05 21:29:16 +0200

    Local adjustments 2025 03 (#10)
    * Data seed
    
    * Mongo DB
    
    * SQL migrations
    
    * development
    
    * dev
    
    * dev

---
+++ db-init-and-seed/20250327201746_OrderChangeIdToGuid.sql
@@ -0,0 +1,10 @@
+USE kinodev_local;
+
+START TRANSACTION;
+
+ALTER TABLE `Orders` MODIFY COLUMN `Id` char(36) COLLATE ascii_general_ci NOT NULL;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20250327201746_OrderChangeIdToGuid', '8.0.2');
+
+COMMIT;
