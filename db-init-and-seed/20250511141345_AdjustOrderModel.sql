65192dab6b30e83f83e3c52411ae922439e98aa9
Andrei Shershan <mail.shershan@gmail.com>
2025-05-11 17:49:21 +0200

    Add migration

---
+++ db-init-and-seed/20250511141345_AdjustOrderModel.sql
@@ -0,0 +1,12 @@
+USE kinodev_local;
+
+START TRANSACTION;
+
+ALTER TABLE `Orders` DROP COLUMN `HashCode`;
+
+ALTER TABLE `Orders` ADD `EmailSent` tinyint(1) NOT NULL DEFAULT FALSE;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20250511141345_AdjustOrderModel', '8.0.2');
+
+COMMIT;
