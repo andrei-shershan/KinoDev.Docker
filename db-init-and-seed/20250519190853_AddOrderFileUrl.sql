51ea0ab35cf248e3bf8ad94bc87d74a7451cc1ce
Andrei Shershan <mail.shershan@gmail.com>
2025-05-20 20:07:32 +0200

    dev

---
+++ db-init-and-seed/20250519190853_AddOrderFileUrl.sql
@@ -0,0 +1,10 @@
+USE kinodev_local; 
+
+START TRANSACTION;
+
+ALTER TABLE `Orders` ADD `FileUrl` longtext CHARACTER SET utf8mb4 NOT NULL;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20250519190853_AddOrderFileUrl', '8.0.2');
+
+COMMIT;
